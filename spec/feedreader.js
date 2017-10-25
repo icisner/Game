ba35eea6ca1d363dc479656b384270a80b032220
Ivan Cisneros <icisner@gmail.com>
2017-10-24 21:35:43 -0500

    update prj

--- spec/feedreader.js
+++ spec/feedreader.js
@@ -17,98 +17,76 @@ $(function() {
         /* This is our first test - it tests to make sure that the
          * allFeeds variable has been defined and that it is not
          * empty. Experiment with this before you get started on
+         * the rest of this project. What happens when you change
          * allFeeds in app.js to be an empty array and refresh the
          * page?
          */
         it('are defined', function() {
             expect(allFeeds).toBeDefined();
             expect(allFeeds.length).not.toBe(0);
-        }); 
-
         });
+
+
         /* TODO: Write a test that loops through each feed
          * in the allFeeds object and ensures it has a URL defined
          * and that the URL is not empty.
          */
-    
+        it('url is defined and not empty', function(){
+            allFeeds.forEach(function(feed) {
+              expect(feed.url).not.toBe();
+            })
+         });
 
-
-
-    describe('allFeeds Check loop name and url',function(){
-    var id=0;
-               
-       function testSpec1(p){
-         it('Name and url are defined in feed item ' + p, function() {
-            expect(allFeeds[p].name.length).not.toBe(0);
-            expect(allFeeds[p].url.length).not.toBe(0);
-        });
-       }
-       for (var i=0; i < allFeeds.length; i++){
-         testSpec1(i);        
-       }
-      });
-
-/*    describe('Check Menu hide',function(){
-    var id=0;
-    var spyEvent;
-
-        beforeEach(function() {
-        setUpHTMLFixture();
-       });
-      it ("should invoke the btnShowMessage click event.", function() {
-          spyEvent = spyOnEvent('#btnShowMessage', 'click');
-          $('#btnShowMessage').trigger( "click" );
-       
-          expect('click').toHaveBeenTriggeredOn('#btnShowMessage');
-          expect(spyEvent).toHaveBeenTriggered();
-        });
-      
-      it ("should invoke the btnHideMessage click event.", function() {
-         spyEvent = spyOnEvent('#btnHideMessage', 'click');
-         $('#btnHideMessage').trigger( "click" );
-       
-         expect('click').toHaveBeenTriggeredOn('#btnHideMessage');
-         expect(spyEvent).toHaveBeenTriggered();
-        });
-      });
-*/
-
-    describe('Check for name and url',function(){
-    var id=0;
- 
-      beforeEach( function(done){
-            loadFeed(id,done);
-        }); 
-       function testSpec(p){
-         it('Name and url are defined in feed item ' + p, function() {
-            expect(allEntries[p].title.length).not.toBe(0);
-            expect(allEntries[p].link.length).not.toBe(0);
-        });
-       }
-      /* allEntries global variable not avilable at the begining */
-        for (var i=0; i < 10;i++){
-         testSpec(i);        
-       }
-      });
-      
         /* TODO: Write a test that loops through each feed
          * in the allFeeds object and ensures it has a name defined
          * and that the name is not empty.
          */
+        it('name is defined and not empty', function(){
+            allFeeds.forEach(function(feed) {
+              expect(feed.name).not.toBe();
+            })
+         });
+
+    });
+
+
     /* TODO: Write a new test suite named "The menu" */
 
         /* TODO: Write a test that ensures the menu element is
-        expect(allEntries[p].title.length).not.toBe(0);
          * hidden by default. You'll have to analyze the HTML and
          * the CSS to determine how we're performing the
          * hiding/showing of the menu element.
          */
+
+        describe('The menu',function(){
+
+          it(' check for hiddem menu', function(){
+
+            expect($('body').attr('class')).toBe('menu-hidden');
+
+          });
+
+
 
          /* TODO: Write a test that ensures the menu changes
           * visibility when the menu icon is clicked. This test
           * should have two expectations: does the menu display when
           * clicked and does it hide when clicked again.
           */
+    
+             it('check for menu display if clicked ',function(){
+
+             $('.icon-list').click();
+              expect($('body').attr('class')).not.toBe('menu-hidden');
+             $('.icon-list').click();
+
+             });
+
+
+
+        });
+
+
 
     /* TODO: Write a new test suite named "Initial Entries" */
 
@@ -119,10 +97,58 @@ $(function() {
          * the use of Jasmine's beforeEach and asynchronous done() function.
          */
 
+
+        describe('Initial Entries',function(){
+          var id=0;
+                 
+          beforeEach( function(done) {
+            loadFeed(id, done);
+          });
+
+          it('There is at least one entry ', function(){
+              
+                 expect($('.feed .entry').length).not.toBe(0);
+          })
+        });
+
+
     /* TODO: Write a new test suite named "New Feed Selection" */
 
         /* TODO: Write a test that ensures when a new feed is loaded
          * by the loadFeed function that the content actually changes.
          * Remember, loadFeed() is asynchronous.
          */
-});
+
+/*        describe('New feed Selection',function(){
+
+        var id=0;
+        var extraEntry={
+          name: 'test',
+          link: 'test_url'
+        };
+        var vlen=0;
+
+                 
+          beforeEach( function(done) {
+            loadFeed(id, done);
+            
+            vlen=allEntries.length;
+            allEntries.push(extraEntry); 
+
+
+          });
+
+          it('check if new feed is loaded  ', function(){
+              
+               //allEntries.forEach(function(feed) {
+
+               expect(allEntries[vlen+1]).not.toBe();
+             //  expect(feed.name).not.toBe(0);
+
+               //  expect($('.feed .entry').length).not.toBe(0);
+                 
+             // });     
+          })
+        });
+*/
+}());
